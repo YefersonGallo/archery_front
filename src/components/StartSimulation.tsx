037ed6c60765216fd5bf92a20cb9340459596eb4
YefersonGallo <yefersongallocastro@gmail.com>
2021-03-09 12:42:05 -0500

    deploy finish (#12)
    * ⚡️Se envían los datos para mostrar estadísticas
    
    * ✨ Se muestra el mejor género por juego
    
    * 🔨Cambio de las rutas
    
    * 🚀Se agregan estadísticas y se deja listo para el despliegue
    
    Co-authored-by: Andrés Gallo <andres.gallo@monolegal.co>

--- src/components/StartSimulation.tsx
+++ src/components/StartSimulation.tsx
@@ -4,11 +4,12 @@ import React, { useState } from 'react';
 type Props = {
   team1: { name?: string, color: SemanticCOLORS, players?: Array<{ 'endurance': number, 'gender': string, 'id': number }> }
   team2: { name?: string, color: SemanticCOLORS, players?: Array<{ 'endurance': number, 'gender': string, 'id': number }> }
-  next: (page: number) => void
+  next: (page: number, stats: any) => void
 }
 
 const StartSimulation = ({ team1, team2, next }: Props) => {
   const [panels, setPanels] = useState<Array<any>>([]);
+  const [statistics, setStatistics] = useState();
   const [buttonName, setButtonName] = useState('Iniciar Juegos');
   const [button, setButton] = useState(false);
   const [view, setView] = useState(true);
@@ -17,15 +18,17 @@ const StartSimulation = ({ team1, team2, next }: Props) => {
     setPanels([]);
     setButton(true);
     setView(true);
-    for (let i = 1; i <= 100; i++) {
+    for (let i = 1; i <= 3; i++) {
       setButtonName(`Juego ${i}`);
       const requestOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ team1, team2 }),
+        body: JSON.stringify({ team1, team2, i }),
       };
-      //const responseP = await fetch('http://localhost:5000/play_round', requestOptions);
-      const responseP = await fetch('https://archery-back.herokuapp.com/play_round', requestOptions);
+      let responseP = await fetch('https://archery-back.herokuapp.com/play_game', requestOptions);
+      while (!responseP.ok){
+        responseP = await fetch('https://archery-back.herokuapp.com/play_game', requestOptions);
+      }
       const res = await responseP.json();
       team1 = res.team_1;
       team2 = res.team_2;
@@ -36,6 +39,12 @@ const StartSimulation = ({ team1, team2, next }: Props) => {
     setView(false);
   };
 
+  const getStats = async () => {
+    const responseP = await fetch('https://archery-back.herokuapp.com/statistics');
+    const res = await responseP.json();
+    setStatistics(res);
+  };
+
   const add_panel = (panel: any) => {
     setPanels(prevState => prevState.concat(panel));
   };
@@ -44,7 +53,7 @@ const StartSimulation = ({ team1, team2, next }: Props) => {
     if (panel['lucky'].id === 0) {
       return 'neuter';
     }
-    return panel['lucky'].gender === 'male' ? 'mars stroke' : 'venus';
+    return panel['lucky'].gender === 'male' ? 'mars' : 'venus';
   };
 
   const getIconGender = (panel: any) => {
@@ -55,7 +64,7 @@ const StartSimulation = ({ team1, team2, next }: Props) => {
       return 'venus';
     }
     if (panel['female'] < panel['male']) {
-      return 'mars stroke';
+      return 'mars';
     }
   };
 
@@ -78,7 +87,10 @@ const StartSimulation = ({ team1, team2, next }: Props) => {
                 onClick={() => initSimulation()}>{buttonName}</Button>
         <div hidden={view}>
           <Button className='button-init' color={'blue'} disabled={button} size={'large'}
-                  onClick={() => next(2)}>Ver Aplicaciones</Button>
+                  onClick={async () => {
+                    await getStats();
+                    next(2, statistics);
+                  }}>Ver Estadísticas</Button>
         </div>
       </Card.Group>
       <Card.Group stackable centered>
@@ -93,7 +105,7 @@ const StartSimulation = ({ team1, team2, next }: Props) => {
                 <Card.Content>
                   <Feed>
                     <Feed.Event>
-                      <Feed.Label icon={panel['winner'].gender === 'male' ? 'mars stroke' : 'venus'} />
+                      <Feed.Label icon={panel['winner'].gender === 'male' ? 'mars' : 'venus'} />
                       <Feed.Content>
                         <Feed.Summary>
                           Ganador individual: Jugador {panel['winner'].id} del equipo {panel['winner'].team}
--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -1,5 +1,95 @@
-const Statistics = () =>{
-  return(<div><h1>Estadísticas</h1></div>)
+import { Accordion, Label, Message } from 'semantic-ui-react';
+import { useEffect, useState } from 'react';
+
+type Props = {
+  page: number
 }
 
-export default Statistics
+const Statistics = ({ page }: Props) => {
+
+  const [info, setInfo] = useState<{ male: number, female: number, team_1: string, total_points_1: number, team_2: string, total_points_2: number }>({
+    female: 0,
+    male: 0,
+    team_1: '',
+    team_2: '',
+    total_points_1: 0,
+    total_points_2: 0,
+  });
+
+  useEffect(() => {
+    async function fetchMyAPI() {
+      const responseIn = await fetch('https://archery-back.herokuapp.com/statistics');
+      const response = await responseIn.json();
+      setInfo(response);
+      console.log(response);
+    }
+
+    if (page === 2 && info.team_1 === '') {
+      fetchMyAPI();
+    }
+  }, [info, page]);
+
+  const panels = [{
+    key: `panel-${1}`,
+    title: {
+      content: <Label color='blue' content={'Victorias Masculinas en total'} icon={'mars'} />,
+    },
+    content: {
+      content: (
+        <Message
+          info
+          header={'Victorias'}
+          content={`Durante los 1000 juegos el género masculino tuvo ${info.male} victorias`}
+        />
+      ),
+    },
+  }, {
+    key: `panel-${2}`,
+    title: {
+      content: <Label color='blue' content={'Victorias Femeninas en total'} icon={'venus'} />,
+    },
+    content: {
+      content: (
+        <Message
+          info
+          header={'Victorias'}
+          content={`Durante los 1000 juegos el género femenino tuvo ${info.female} victorias`}
+        />
+      ),
+    },
+  }, {
+    key: `panel-${3}`,
+    title: {
+      content: <Label color='blue' content={info.team_1} icon={'users'} />,
+    },
+    content: {
+      content: (
+        <Message
+          info
+          header={'Victorias'}
+          content={`Durante los 1000 juegos el equipo ${info.team_1} obtuvo ${info.total_points_1} puntos`}
+        />
+      ),
+    },
+  }, {
+    key: `panel-${4}`,
+    title: {
+      content: <Label color='blue' content={info.team_2} icon={'users'} />,
+    },
+    content: {
+      content: (
+        <Message
+          info
+          header={'Victorias'}
+          content={`Durante los 1000 juegos el equipo ${info.team_2} obtuvo ${info.total_points_2} puntos`}
+        />
+      ),
+    },
+  }];
+
+  return (
+    <Accordion panels={panels} styled />
+  );
+};
+
+export default Statistics;
